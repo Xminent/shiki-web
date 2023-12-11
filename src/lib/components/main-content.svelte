95126ef68cbee49832b59ee316668e9ebf4ae112
Xminent <mail@xminent.com>
2023-12-11 17:11:03 -0600

    feat: :sparkles: add file picker input to message input (bare)

--- src/lib/components/main-content.svelte
+++ src/lib/components/main-content.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { Plus } from 'lucide-svelte';
 	import type { Message, SidebarItem } from '../../types/sidebar';
 	import MessageItem from './message-item.svelte';
+	import Label from './ui/label/label.svelte';
 
 	export let item: SidebarItem;
 	export let messages: Message[];
@@ -17,6 +19,16 @@
 
 	$: {
 		textareaRows = Math.min(inputText.split('\n').length, 10);
+	}
+
+	let files: FileList;
+
+	$: if (files) {
+		console.log(files);
+
+		for (const file of files) {
+			console.log(`${file.name}: ${file.size} bytes`);
+		}
 	}
 </script>
 
@@ -37,35 +49,49 @@
 		</ul>
 	</section>
 	<section class="p-4 w-full">
-		<textarea
-			class="w-full px-4 py-2 text-sm bg-gray-200 dark:bg-zinc-900 rounded-md focus:outline-none text-black dark:text-white resize-none"
-			rows={textareaRows}
-			bind:value={inputText}
-			placeholder={`Message #${item.name}`}
-			on:keydown={(e) => {
-				const text = inputText.trim();
+		<div class="flex flex-row">
+			<div
+				class="flex flex-row justify-center items-center w-[48px] bg-gray-200 dark:bg-zinc-900 rounded-tl-md rounded-bl-md"
+			>
+				<Label for="files">
+					<div
+						class="flex justify-center-items-center rounded-full bg-gray-100 dark:bg-zinc-500 cursor-pointer hover:bg-gray-300 dark:hover:bg-zinc-400"
+					>
+						<Plus color="black" class="w-5 h-5" />
+					</div>
+				</Label>
+				<input bind:files multiple id="files" type="file" class="hidden" />
+			</div>
+			<textarea
+				class="w-full px-4 py-2 text-sm bg-gray-200 dark:bg-zinc-900 rounded-tr-md rounded-br-md focus:outline-none text-black dark:text-white resize-none"
+				rows={textareaRows}
+				bind:value={inputText}
+				placeholder={`Message #${item.name}`}
+				on:keydown={(e) => {
+					const text = inputText.trim();
 
-				if (e.key === 'Enter') {
-					if (e.shiftKey) {
+					if (e.key === 'Enter') {
+						if (e.shiftKey) {
+							return;
+						}
+
+						if (!text) {
+							e.preventDefault();
+							return;
+						}
+					}
+
+					if (e.key !== 'Enter' || e.shiftKey || !text) {
 						return;
 					}
 
-					if (!text) {
-						e.preventDefault();
-						return;
-					}
-				}
-
-				if (e.key !== 'Enter' || e.shiftKey || !text) {
-					return;
-				}
-
-				e.preventDefault();
-				onSendMessage(text);
-				inputText = '';
-				scrollToBottom(messageList);
-			}}
-		/>
+					e.preventDefault();
+					onSendMessage(text);
+					inputText = '';
+					scrollToBottom(messageList);
+				}}
+			/>
+		</div>
 	</section>
 </div>
 
