d92f6785cfb08090f7d1f6675aba7c3beb07a7e2
Xminent <mail@xminent.com>
2023-12-12 19:41:08 -0600

    ci: :green_heart: add privacy route

---
+++ src/routes/(marketing)/privacy/+page.svelte
@@ -0,0 +1,20 @@
+<main class="container grid items-center gap-6 pb-8 pt-6 md:py-10 max-w-screen-md">
+	<h1 class="text-3xl font-extrabold uppercase leading-tight tracking-tighter md:text-4xl">
+		Privacy Policy
+	</h1>
+	<p class="text-lg text-muted-foreground">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus
+		tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
+		Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus
+		tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
+		Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+	</p>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus
+		tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.
+		Maecenas ligula massa, varius a, semper congue, euismod non, mi.
+	</p>
+</main>
